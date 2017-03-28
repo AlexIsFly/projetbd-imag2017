06b4c8299754249bfa27e639e55577197262556c
Alexis Gacel <alexis.gacel@phelma.grenoble-inp.fr>
2017-03-28 11:00:43 +0200

    Removed some files

--- src/JDBCMain.java
+++ src/JDBCMain.java
@@ -2,16 +2,10 @@ import javax.swing.*;
 import java.sql.*;
 
 public class JDBCMain {
-	
-	static final String CONN_URL = "jdbc:oracle:thin:@ensioracle1.imag.fr:1521:ensioracle1";
-    static final String USER = "scott";
-    static final String PASSWD = "tiger";
-    static final String PRE_STMT = "select * from emp";
     
     public static void main(String[] args) {
-    	
+
         SportQuery sportQuery = new SportQuery();
-        Connexion connec = new Connexion();
 
     }
 }
