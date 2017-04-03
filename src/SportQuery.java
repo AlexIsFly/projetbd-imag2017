8b1137852fdc5d8979f7258d45fa21fc51b34e5c
Alexis Gacel <alexis.gacel@phelma.grenoble-inp.fr>
2017-04-03 14:46:29 +0200

    Antoine says override are poop

--- src/SportQuery.java
+++ src/SportQuery.java
@@ -94,17 +94,14 @@ public class SportQuery extends JFrame implements RowSetListener {
         return rset;
     }
 
-    @Override
     public void rowSetChanged(RowSetEvent event) {
 
     }
 
-    @Override
     public void rowChanged(RowSetEvent event) {
 
     }
 
-    @Override
     public void cursorMoved(RowSetEvent event) {
 
     }
