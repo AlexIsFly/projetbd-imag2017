bf206af1a048469c1113879e180356914dd549ad
Alexis Gacel <alexis.gacel@phelma.grenoble-inp.fr>
2017-04-07 23:14:10 +0200

    Added a box to print opening and closing time

--- src/RajoutStageUI.java
+++ src/RajoutStageUI.java
@@ -28,7 +28,17 @@ public class RajoutStageUI extends JPanel implements ActionListener {
     JComboBox<String> terrainList;
     JLabel terrainLabel;
 
+    Calendar selectedDay;
+    Box timeBox = new Box(BoxLayout.LINE_AXIS);
     Box horaireBox = new Box(BoxLayout.LINE_AXIS);
+
+    JLabel startTime;
+    JTextField startHours;
+    JTextField startMinutes;
+    JLabel endTime;
+    JTextField endHours;
+    JTextField endMinutes;
+    JButton verifyTime;
 
     JLabel date;
     JXDatePicker picker;
@@ -40,6 +50,24 @@ public class RajoutStageUI extends JPanel implements ActionListener {
         this.picker.setFormats(new SimpleDateFormat("dd.MM.yyyy"));
         this.picker.addActionListener(this);
         this.date = new JLabel("Date");
+        this.selectedDay = Calendar.getInstance();
+
+        this.startTime = new JLabel("Début hh:mm");
+        this.startHours = new JTextField(2);
+        this.startMinutes = new JTextField(2);
+        this.endTime = new JLabel("Fin hh:mm");
+        this.endHours = new JTextField(2);
+        this.endMinutes = new JTextField(2);
+        this.verifyTime = new JButton("Verify");
+        this.verifyTime.addActionListener(this);
+        this.timeBox.add(startTime);
+        this.timeBox.add(startHours);
+        this.timeBox.add(startMinutes);
+        this.timeBox.add(endTime);
+        this.timeBox.add(endHours);
+        this.timeBox.add(endMinutes);
+        this.timeBox.add(verifyTime);
+
 
         this.connectionBD = connectionBD;
         map = new HashMap<String, String>();
@@ -66,6 +94,7 @@ public class RajoutStageUI extends JPanel implements ActionListener {
         add(this.horaireBox);
         add(this.date);
         add(this.picker);
+        add(this.timeBox);
     }
 
     private String[] getSportList() throws SQLException {
@@ -118,11 +147,23 @@ public class RajoutStageUI extends JPanel implements ActionListener {
         String terrain_com = map.get("terrain");
         String terrain = terrain_com.split(" - ")[0];
         String commune = terrain_com.split(" - ")[1];
-        String PRE_STMT1 = "select heureouverture, heurefermeture from TERRAIN where NOMTERRAIN =\'" + terrain + "\' AND COMMUNE = \'" + commune + "\';";
+        String PRE_STMT1 = "select heureouverture, heurefermeture from TERRAIN where NOMTERRAIN ='" + terrain + "' AND COMMUNE = '" + commune + "'";
         System.out.println("PRE_STMT1 = " + PRE_STMT1);
         PreparedStatement stmt = conn.prepareStatement(PRE_STMT1);
         ResultSet rset = stmt.executeQuery();
-        String horaire = "Le terrain " + terrain + "est ouvert de " + rset.getString(1) + " à " + rset.getString(2);
+        rset.next();
+        Date tempDate = new Date(rset.getDate(1).getTime());
+        Calendar tempCal = Calendar.getInstance();
+        tempCal.setTime(tempDate);
+
+        String horaire = "Le terrain " + terrain + " est ouvert de "
+                + tempCal.get(Calendar.HOUR_OF_DAY) + ":"
+                + tempCal.get(Calendar.MINUTE);
+
+        tempDate = new Date(rset.getDate(2).getTime());
+        tempCal.setTime(tempDate);
+        horaire += " à " + tempCal.get(Calendar.HOUR_OF_DAY) + ":"
+                + tempCal.get(Calendar.MINUTE);
         System.out.println(horaire);
         stmt.close();
         System.out.println("Stmt closed.");
@@ -172,7 +213,10 @@ public class RajoutStageUI extends JPanel implements ActionListener {
         if (e.getSource() == picker){
             Date selectedDate = this.picker.getDate();
             map.put("date",selectedDate.toString());
-            System.out.println("selectedDate = " + map.get("date"));
+            this.selectedDay.setTime(selectedDate);
+            System.out.println("selectedDate = " + this.selectedDay.toString());
+        }
+        if (e.getSource() == verifyTime){
         }
     }
 }
